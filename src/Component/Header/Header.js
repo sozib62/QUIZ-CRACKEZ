8381a098182d2b0ed2cdda83224b92c6b48dd991
sozib62 <webdevsozib62@gmail.com>
2022-10-11 11:29:50 +0600

    added header

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -5,9 +5,14 @@ import './Header.css'
 const Header = () => {
     return (
         <nav className='header'>
-            <Link to='/'>Home</Link>
-            <Link to='/Quiz'>Quiz</Link>
-            <Link to='/Blog'>Blog</Link>
+            <div>
+                <h2>Select Quiz</h2>
+            </div>
+            <div className='link'>
+                <Link to='/'>Home</Link>
+                <Link to='/Quiz'>Quiz</Link>
+                <Link to='/Blog'>Blog</Link>
+            </div>
         </nav>
     );
 };
